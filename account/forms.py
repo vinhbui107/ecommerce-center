efda8149d15c0d29b4a8b2bb28949c34fc613a88
lethienbao99 <1754050012bao@ou.edu.vn>
2020-04-18 21:09:20 +0700

    form signup

---
+++ account/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+import re
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth import get_user_model
+from captcha.fields import CaptchaField
+
+User = get_user_model()
+
+
+class RegistrationForm(forms.Form):
+    username = forms.CharField(label='Tài Khoản', max_length=30)
+    email = forms.EmailField(label='Email')
+    password1 = forms.CharField(label='Mật khẩu', widget=forms.PasswordInput())
+    password2 = forms.CharField(label='Nhập lại mật khẩu', widget=forms.PasswordInput())
+    captcha = CaptchaField(label='Captcha:')
+
+    def clean_password2(self):
+        if 'password1' in self.cleaned_data:
+            password1 = self.cleaned_data['password1']
+            password2 = self.cleaned_data['password2']
+            if password1 == password2 and password1:
+                return password2
+            raise forms.ValidationError("Mật khẩu không hợp lệ")
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if not re.search(r'^\w+$', username):
+            raise forms.ValidationError("Tên tài khoản có ký tự đặc biệt")
+        try:
+            User.objects.get(username=username)
+        except ObjectDoesNotExist:
+            return username
+        raise forms.ValidationError("Tài khoản đã tồn tại")
+
+    def save(self):
+        User.objects.create_user(username=self.cleaned_data['username'], email=self.cleaned_data['email'], password=self.cleaned_data['password1'])
