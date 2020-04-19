95a7520d2d2a45ecbfb8d8d32846371bf6e0c8f8
Vinh Bui <vinhbui.tg@gmail.com>
2020-04-20 02:48:35 +0700

    update example view

--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-
 from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+
+class ContactView(TemplateView):
+    template_name = 'contact.html'
+
+
+class MovieDetailView(TemplateView):
+    template_name = "movie/movie_detail.html"
