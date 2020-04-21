1cab1d3667c78e100b8107e617eca6b824af1bd6
Vinh Bui <vinhbui.tg@gmail.com>
2020-04-21 18:28:55 +0700

    detele files to config folder

--- account/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import CustomerUser
-
-
-admin.site.register(CustomerUser)
--- account/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class CustomerUser(AbstractUser):
-    address = models.CharField(default='', max_length=255)
-    phone_number = models.CharField(default='', max_length=10)
