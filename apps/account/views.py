0fc6e89051c1b377d4d80367428330135d065cbc
Vinh Bui <vinhbui.tg@gmail.com>
2020-04-23 23:33:30 +0700

    change name files link to template

--- apps/account/views.py
+++ apps/account/views.py
@@ -2,49 +2,36 @@ from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth import update_session_auth_hash
-from django.contrib import messages
 from django.contrib.auth.forms import PasswordChangeForm
-from django.views.generic import FormView
+from django.views.generic import FormView, CreateView
 from .forms import LoginForm, SignUpForm, UpdateProfileForm
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
-from django.views import generic
 
 
+@login_required
 def profile(request):
     user = request.user
     if request.method == "POST":
         form = UpdateProfileForm(request.POST, instance=user, files=request.FILES)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect("/account/successprofile")
+            return HttpResponseRedirect("/account/profile/success")
     else:
         form = UpdateProfileForm(instance=user)
 
-    return render(request, "account/profile.html", {"form": form})
+    return render(request, "account/profile_update.html", {"form": form})
 
 
-def successprofile(request):
-    return render(request, "account/successprofile.html")
+def profile_success(request):
+    return render(request, "account/profile_success.html")
 
 
 class SiteLoginView(LoginView):
     template_name = "account/login.html"
 
 
-class SiteSignupView(generic.CreateView):
+class SiteSignupView(CreateView):
     template_name = "account/signup.html"
-    success_url = reverse_lazy("login")
     form_class = SignUpForm
-
-
-class UpdateProfileView:
-    pass
-
-
-class UpdateProfileDoneView:
-    pass
-
-
-class ChangePasswordFailView:
-    pass
+    success_url = reverse_lazy("login")
