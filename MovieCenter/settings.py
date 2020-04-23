8d3395f38b7b0e1a2b0b103ab8ef3c8af43beb32
Vinh Bui <vinhbui.tg@gmail.com>
2020-04-23 23:39:31 +0700

    add debug feature

--- MovieCenter/settings.py
+++ MovieCenter/settings.py
@@ -71,6 +71,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 # ==============================================================================
@@ -169,4 +170,4 @@ DEFAULT_FROM_EMAIL = "MovieCenter <noreply@MovieCenter.com>"
 # THIRD-PARTY APPS SETTINGS
 # ==============================================================================
 CRISPY_TEMPLATE_PACK = "bootstrap4"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
