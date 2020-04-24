6b181b5115faa43c4a21971b72e7de59da42fbbe
Vinh Bui <vinhbui.tg@gmail.com>
2020-04-24 12:21:43 +0700

    add new urls for movie

--- MovieCenter/urls.py
+++ MovieCenter/urls.py
@@ -18,3 +18,15 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(), name="logout"),
     path("signup/", SiteSignupView.as_view(), name="signup"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    try:
+        import debug_toolbar
+
+        urlpatterns = [path("__debug__/", include(debug_toolbar.urls)),] + urlpatterns
+    except ImportError:
+        pass
