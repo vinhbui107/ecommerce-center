c075f8cef642e4c26a2a6dfaf0686a11b7999ef9
Vinh Bui <vinhbui.tg@gmail.com>
2020-04-24 12:23:08 +0700

    create detail and list movie view

--- apps/movie/views.py
+++ apps/movie/views.py
@@ -1,5 +1,18 @@
-from django.views.generic import TemplateView
+from django.views.generic import DetailView, ListView
+
+from .models import Movie
 
 
-class MovieDetailView(TemplateView):
+class MovieDetailView(DetailView):
+    model = Movie
+    slug_url_kwarg = "the_slug"
+    context_object_name = "movie"
+    slug_field = "slug"
     template_name = "movie/movie_detail.html"
+
+
+class MovieListView(ListView):
+    model = Movie
+    context_object_name = "movie_list"
+    template_name = "movie/movie_list.html"
+    paginate_by = 10
