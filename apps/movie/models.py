ec1fdf2a58f30296229c5e9dd0c021ddfd83326c
lethienbao99 <1754050012bao@ou.edu.vn>
2020-04-26 01:07:06 +0700

    update models movie

--- apps/movie/models.py
+++ apps/movie/models.py
@@ -35,3 +35,9 @@ class Movie(models.Model):
 
     def get_absolute_url(self):
         return reverse("movie-detail", kwargs={"slug": self.slug})
+
+    def get_add_to_cart_url(self):
+        return reverse("cart:add-to-cart", kwargs={"slug": self.slug})
+
+    def get_remove_from_cart_url(self):
+        return reverse("cart:remove_from_cart", kwargs={"slug": self.slug})
