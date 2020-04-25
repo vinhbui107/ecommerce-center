3d38b2994c77858c35942884bef1d9f62ee06e58
lethienbao99 <1754050012bao@ou.edu.vn>
2020-04-26 01:05:56 +0700

    update urls, views, db

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
 from django.conf.urls import url
-from .views import CartDetailView
+from .views import (
+    CartDetailView,
+    add_to_cart,
+    remove_from_cart,
+    remove_single_item_from_cart,
+    add_simple_item_to_cart
+)
 
 app_name = "cart"
 
-urlpatterns = [path("test/", CartDetailView.as_view(), name="cart")]
+urlpatterns = [
+    path("yourcart/", CartDetailView.as_view(), name="cart"),
+    path("add-to-cart/<slug>/", add_to_cart, name="add-to-cart"),
+    path("remove-from-cart/<slug>/", remove_from_cart, name="remove_from_cart"),
+    path("remove-single-item-from-cart/<slug>/", remove_single_item_from_cart, name="remove-single-item-from-cart"),
+    path("add-simple-item-to-cart/<slug>/", add_simple_item_to_cart, name="add-simple-item-to-cart"),
+]
--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,5 +1,135 @@
-from django.views.generic import TemplateView
+from django.views.generic import View
+from django.shortcuts import redirect, render, get_object_or_404
+from apps.movie.models import Movie
+from apps.cart.models import Cart, OrderMovie
+from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class CartDetailView(TemplateView):
-    template_name = "cart/cart.html"
+class CartDetailView(LoginRequiredMixin, View):
+    def get(self, *args, **kwargs):
+        try:
+            order = Cart.objects.get(user_id=self.request.user, ordered=False)
+            context = {
+                'movie': order
+            }
+            return render(self.request, "cart/cart.html", context)
+        except ObjectDoesNotExist:
+            messages.error(self.request, "You do not have an active order")
+            return redirect("/")
+
+
+@login_required
+def add_to_cart(request, slug):
+    item = get_object_or_404(Movie, slug=slug)
+    order_item, created = OrderMovie.objects.get_or_create(
+        item=item,
+        user_id=request.user,
+        ordered=False
+    )
+    order_qs = Cart.objects.filter(user_id=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+            messages.info(request, "This item quantity was update")
+            return redirect("movie:movie-detail", the_slug=slug)
+        else:
+            order.items.add(order_item)
+            messages.info(request, "This item was added to your cart")
+            return redirect("movie:movie-detail", the_slug=slug)  # movie:movie-detail
+
+    else:
+        # update_date = timezone.now()
+        order = Cart.objects.create(user_id=request.user)
+        order.items.add(order_item)
+        messages.info(request, "This item was added to your cart")
+    return redirect("movie:movie-detail", the_slug=slug)
+
+
+@login_required
+def remove_from_cart(request, slug):
+    item = get_object_or_404(Movie, slug=slug)
+    order_qs = Cart.objects.filter(
+        user_id=request.user,
+        ordered=False
+    )
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderMovie.objects.filter(
+                item=item,
+                user_id=request.user,
+                ordered=False
+            )[0]
+            order.items.remove(order_item)
+            order_item.delete()
+            messages.info(request, "This item was removed to your cart")
+            return redirect("cart:cart")
+        else:
+            messages.info(request, "This item was not in your cart")
+            return redirect("movie:movie-detail", the_slug=slug)
+    else:
+        messages.info(request, "You do not have an active order")
+        return redirect("movie:movie-detail", the_slug=slug)
+
+
+@login_required
+def remove_single_item_from_cart(request, slug):
+    item = get_object_or_404(Movie, slug=slug)
+    order_qs = Cart.objects.filter(
+        user_id=request.user,
+        ordered=False
+    )
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderMovie.objects.filter(
+                item=item,
+                user_id=request.user,
+                ordered=False
+            )[0]
+            if order_item.quantity > 1:
+                order_item.quantity -= 1
+                order_item.save()
+            else:
+                order.items.remove(order_item)
+            messages.info(request, "This item quantity was updated.")
+            return redirect("cart:cart")
+        else:
+            messages.info(request, "This item was not in your cart")
+            return redirect("movie:movie-detail", the_slug=slug)
+    else:
+        messages.info(request, "You do not have an active order")
+        return redirect("movie:movie-detail", the_slug=slug)
+
+
+@login_required
+def add_simple_item_to_cart(request, slug):
+    item = get_object_or_404(Movie, slug=slug)
+    order_item, created = OrderMovie.objects.get_or_create(
+        item=item,
+        user_id=request.user,
+        ordered=False
+    )
+    order_qs = Cart.objects.filter(user_id=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+            messages.info(request, "This item quantity was update")
+            return redirect("cart:cart")
+        else:
+            order.items.add(order_item)
+            messages.info(request, "This item was added to your cart")
+            return redirect("cart:cart")
+    else:
+        # update_date = timezone.now()
+        order = Cart.objects.create(user_id=request.user)
+        order.items.add(order_item)
+        messages.info(request, "This item was added to your cart")
+    return redirect("cart:cart")
