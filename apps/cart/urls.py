7f5fbc9adabd162a97a67b42fad069c0853e71e6
Vinh Bui <vinhbui.tg@gmail.com>
2020-04-26 23:36:00 +0700

    delete urls unuse

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,19 +1,10 @@
 from django.urls import path
-from django.conf.urls import url
-from .views import (
-    CartDetailView,
-    add_to_cart,
-    remove_from_cart,
-    remove_single_item_from_cart,
-    add_simple_item_to_cart
-)
+from .views import CartDetailView, add_to_cart, remove_from_cart
 
 app_name = "cart"
 
 urlpatterns = [
-    path("yourcart/", CartDetailView.as_view(), name="cart"),
-    path("add-to-cart/<slug>/", add_to_cart, name="add-to-cart"),
-    path("remove-from-cart/<slug>/", remove_from_cart, name="remove_from_cart"),
-    path("remove-single-item-from-cart/<slug>/", remove_single_item_from_cart, name="remove-single-item-from-cart"),
-    path("add-simple-item-to-cart/<slug>/", add_simple_item_to_cart, name="add-simple-item-to-cart"),
+    path("", CartDetailView.as_view(), name="cart"),
+    path("add/<slug>/", add_to_cart, name="add-to-cart"),
+    path("remove/<slug>/", remove_from_cart, name="remove-from-cart"),
 ]
