6d0c634d3ff86dcf3a7be643f5ff55da4c5d212e
Vinh Bui <vinhbui.tg@gmail.com>
2020-04-26 23:35:32 +0700

    fix and delete fields unuse

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -1,36 +1,30 @@
 from django.db import models
 from apps.movie.models import Movie
 from apps.account.models import CustomerUser
+import datetime
 
 
-class OrderMovie(models.Model):
+class CartMovie(models.Model):
     user_id = models.ForeignKey(CustomerUser, on_delete=models.CASCADE)
-    ordered = models.BooleanField(default=False)
-    item = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=1)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
 
-    def __str__(self):
-        return f"{self.quantity} of {self.item.title}"
-
-    def get_total_item_price(self):
-        return self.quantity * self.item.price
-
-    def get_amount_saved(self):
-        return self.get_total_item_price()
+    def get_movie_price(self):
+        return self.movie.price
 
 
 class Cart(models.Model):
+    user_id = models.ForeignKey(CustomerUser, on_delete=models.CASCADE)
+    movies = models.ManyToManyField(CartMovie)
     create_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
-    user_id = models.ForeignKey(CustomerUser, on_delete=models.CASCADE)
-    items = models.ManyToManyField(OrderMovie)
-    ordered = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.user_id.username
+        return "Cart Id: %s by user: %s" % (self.id, self.user_id)
 
     def get_total(self):
         total = 0
-        for order_item in self.items.all():
-            total += order_item.get_amount_saved()
+        for movie in self.movies.all():
+            total += movie.get_movie_price()
         return total
