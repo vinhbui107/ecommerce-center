22f062353a346fe236078c03a9ffe5cea4f63e08
Vinh Bui <vinhbui.tg@gmail.com>
2020-04-27 22:44:15 +0700

    add new urls for checkout app

--- apps/checkout/urls.py
+++ apps/checkout/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.conf.urls import url
+from apps.checkout.views import CheckoutView, CheckoutDoneView
 
-app_name = 'checkout'
+app_name = "checkout"
 
 urlpatterns = [
-
+    path("", CheckoutView.as_view(), name="checkout"),
+    path("done/", CheckoutDoneView.as_view(), name="checkout-done"),
 ]
