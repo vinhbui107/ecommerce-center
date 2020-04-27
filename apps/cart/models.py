f7c8186ca1fe66d0c9bf93274d297e5f34569637
Vinh Bui <vinhbui.tg@gmail.com>
2020-04-27 22:41:10 +0700

    fix update_date for models

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from apps.movie.models import Movie
 from apps.account.models import CustomerUser
 import datetime
+from django.utils import timezone
 
 
 class CartMovie(models.Model):
@@ -16,15 +17,19 @@ class CartMovie(models.Model):
 class Cart(models.Model):
     user_id = models.ForeignKey(CustomerUser, on_delete=models.CASCADE)
     movies = models.ManyToManyField(CartMovie)
-    create_date = models.DateTimeField(auto_now_add=True)
-    update_date = models.DateTimeField(auto_now=True)
+    create_date = models.DateTimeField(default=timezone.now, editable=False)
+    update_date = models.DateTimeField(default=timezone.now)
     is_active = models.BooleanField(default=True)
-
-    def __str__(self):
-        return "Cart Id: %s by user: %s" % (self.id, self.user_id)
 
     def get_total(self):
         total = 0
         for movie in self.movies.all():
             total += movie.get_movie_price()
         return total
+
+    def save(self, *args, **kwargs):
+        """ On save, update timestamps """
+        if not self.id:
+            self.created = timezone.now()
+        self.update_date = timezone.now()
+        return super(Cart, self).save(*args, **kwargs)
--- apps/checkout/models.py
+++ apps/checkout/models.py
@@ -1,13 +1,24 @@
 from django.db import models
 from apps.account.models import CustomerUser
 from apps.cart.models import Cart
+from django.utils import timezone
 
 
 class Order(models.Model):
     user_id = models.ForeignKey(CustomerUser, on_delete=models.CASCADE)
     cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    fullname = models.CharField(max_length=100, null=True)
     shipping_address = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=10)
     order_description = models.CharField(default="", max_length=255)
     order_total = models.DecimalField(max_digits=9, decimal_places=2)
     is_complete = models.BooleanField(default=False)
+    create_date = models.DateTimeField(default=timezone.now, editable=False)
+    update_date = models.DateTimeField(default=timezone.now)
+
+    def save(self, *args, **kwargs):
+        """ On save, update timestamps """
+        if not self.id:
+            self.create = timezone.now()
+        self.update_date = timezone.now()
+        return super(Order, self).save(*args, **kwargs)
