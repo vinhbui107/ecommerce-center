0fa21bcb2dfa367b66bc2685222b266fdf9bea17
lethienbao99 <1754050012bao@ou.edu.vn>
2020-05-04 18:39:00 +0700

    valid form signup( username + email)

--- apps/account/forms.py
+++ apps/account/forms.py
@@ -10,6 +10,7 @@ from django.contrib.auth.forms import (
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from .models import CustomerUser
+import re
 
 
 class LoginForm(forms.Form):
@@ -21,6 +22,20 @@ class SignUpForm(UserCreationForm):
         model = CustomerUser
         fields = ("username", "email", "address", "phone_number")
         field_classes = {"username": UsernameField}
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if not re.search(r'^\w+$', username):
+            raise forms.ValidationError("Letters and numbers only. Please try again without symbols.")
+        if len(username) < 8:
+            raise forms.ValidationError("Username has to be longer then 8 characters.")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if CustomerUser.objects.filter(email=email).exists():
+            raise forms.ValidationError('Your email address is not unique')
+        return email
 
 
 class UpdateProfileForm(forms.ModelForm):
