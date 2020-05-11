b30d6947b3a2e27fec68f3842c07ece91cd9da44
vinhbui107 <vinhbui.tg@gmail.com>
2020-05-11 18:34:22 +0700

    fix check valid form

--- apps/checkout/forms.py
+++ apps/checkout/forms.py
@@ -12,18 +12,38 @@ class CheckoutForm(forms.ModelForm):
 
     def clean_fullname(self):
         fullname = self.cleaned_data['fullname']
-        if not re.search(r'^\w+$', fullname):
+
+        string_is_valid = check_symbol(fullname)
+
+        if string_is_valid:
+            return fullname
+        else:
             raise forms.ValidationError("Letters and numbers only. Please try again without symbols.")
-        return fullname
 
     def clean_shipping_address(self):
         shipping_address = self.cleaned_data['shipping_address']
-        if not re.search(r'^\w+$', shipping_address):
+
+        string_is_valid = check_symbol(shipping_address)
+
+        if string_is_valid:
+            return shipping_address
+        else:
             raise forms.ValidationError("Letters and numbers only. Please try again without symbols.")
-        return shipping_address
 
     def clean_order_description(self):
         order_description = self.cleaned_data['order_description']
-        if not re.search(r'^\w+$', order_description):
+        string_is_valid = check_symbol(order_description)
+
+        if string_is_valid:
+            return order_description
+        else:
             raise forms.ValidationError("Letters and numbers only. Please try again without symbols.")
-        return order_description
+
+
+def check_symbol(string):
+    regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+
+    if(regex.search(string) == None):
+        return True
+    else:
+        return False
